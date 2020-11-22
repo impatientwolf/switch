556b287deb2de9b30a0b10e48e90e9dd08dad166
impatientwolf <74821555+impatientwolf@users.noreply.github.com>
2020-11-22 23:16:45 +0530

    before deadline

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,6 +10,7 @@ package sample;
         import javafx.scene.Parent;
         import javafx.scene.Scene;
         import javafx.scene.image.ImageView;
+        import javafx.scene.shape.Arc;
         import javafx.scene.shape.Circle;
         import javafx.stage.Stage;
         import javafx.util.Duration;
@@ -39,6 +40,16 @@ public class Controller implements Initializable {
     private ImageView saveImage1;
     @FXML
     private ImageView saveImage;
+    @FXML
+    private Arc a1;
+    @FXML
+    private Arc a2;
+    @FXML
+    private Arc a3;
+    @FXML
+    private Arc a4;
+    @FXML
+    private Circle smallC;
 
 
     @Override
@@ -46,16 +57,31 @@ public class Controller implements Initializable {
         setRotate(c1, true, 360, 10);
         setRotate(c2, false, 360, 10);
         setRotate(c3, true, 360, 10);
+        setRotate(a1, true, 360, 10);
+        setRotate(a2, true, 360, 10);
+        setRotate(a3, true, 360, 10);
+        setRotate(a4, true, 360, 10);
+        setRotate(smallC, true, 360, 10);
+
         setRotate(playImage, false, 360, 10);
         setRotate(playImage1, false, 360, 10);
         setRotate(settingsImage, true, 360, 10);
         setRotate(loadImage, true, 360, 10);
         setRotate(saveImage, true, 360, 10);
-        setRotate(saveImage1, true, 360, 10);
+        setRotate(saveImage1, true, 360, 15);
 
     }
 
     private void setRotate(Circle c, boolean reverse, int angle, int duration) {
+
+        RotateTransition rotateTransition = new RotateTransition(Duration.seconds(duration), c);
+        rotateTransition.setAutoReverse(reverse);
+        rotateTransition.setByAngle(angle);
+        rotateTransition.setRate(7);
+        rotateTransition.setCycleCount(90);
+        rotateTransition.play();
+    }
+    private void setRotate(Arc c, boolean reverse, int angle, int duration) {
 
         RotateTransition rotateTransition = new RotateTransition(Duration.seconds(duration), c);
         rotateTransition.setAutoReverse(reverse);
